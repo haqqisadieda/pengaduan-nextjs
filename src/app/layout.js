5473c42ad37b1e1d7e999f0b731e7e40486a6d34
haqqi <haqqisadiedapandega@gmail.com>
2025-08-30 08:06:26 +0700

    final dev

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import icon from '../../public/logo_bapenda.png';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
